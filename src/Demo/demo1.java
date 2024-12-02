baad9c892df29166cb6751f3905c62aaf92ebe16
husainbhaldar1986 <husainbhaldar1986@gmail.com>
2024-12-02 19:33:37 +0530

    commit

---
+++ src/Demo/demo1.java
@@ -0,0 +1,12 @@
+package Demo;
+
+public class demo1 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Demo 1 class running");
+
+	}
+
+}
---
+++ src/Demo/demo2.java
@@ -0,0 +1,12 @@
+package Demo;
+
+public class demo2 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Demo 2 class running");
+
+	}
+
+}
